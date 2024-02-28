2907ce455f97099371a5feedf24ac1b31d55469c
Juliana Verneck <150185892+Verneckjuliana@users.noreply.github.com>
2024-02-28 09:44:17 -0300

    Add files via upload

--- biblioteca_banco.sql
+++ biblioteca_banco.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE biblioteca;
+CREATE DATABASE bibliotecaLivros;
 
 CREATE TABLE funcionarios(
 	registro INT AUTO_INCREMENT PRIMARY KEY,
@@ -7,19 +7,18 @@ CREATE TABLE funcionarios(
 );
 
 CREATE TABLE livros(
+	id_livro INT,
 	titulo VARCHAR(255) NOT NULL,
+    autor TEXT,
     ISBN INT AUTO_INCREMENT PRIMARY KEY,
     descricao TEXT,
-    imagem BLOB,
     categoria ENUM('ficção','romance','mistério')
 );
 
 CREATE TABLE autores(
 	nome VARCHAR(255) NOT NULL,
     data_nascimento DATE,
-    biografia TEXT,
-    titulo VARCHAR(255) NOT NULL,
-    FOREIGN KEY (titulo) REFERENCES livros (titulo)
+    biografia TEXT
 );
 
 CREATE TABLE usuarios(
@@ -27,14 +26,21 @@ CREATE TABLE usuarios(
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE,
 	data_registro DATE,
-    associacao ENUM('regular','premium')
+    associacao ENUM('regular','premium')PRIMARY
 );
 
 CREATE TABLE emprestimos(
-	nome VARCHAR(255) NOT NULL,
-	FOREIGN KEY (nome) REFERENCES usuarios (nome),
+	ISBN INT,
     data_inicio DATE,
     data_retorno DATE,
-    titulo VARCHAR(255) NOT NULL,
-    FOREIGN KEY (titulo) REFERENCES livros (titulo)
+    FOREIGN KEY (ISBN) REFERENCES livros (ISBN)
 );
+ 
+ALTER TABLE emprestimos
+DROP COLUMN nome_da_coluna;
+
+
+SELECT livros.titulo, usuarios.nome, emprestimos.data_inicio, emprestimos.data_retorno
+FROM emprestimos
+INNER JOIN livros ON emprestimos.ISBN = livros.ISBN
+INNER JOIN usuarios ON emprestimos.id_usuario = usuarios.id;
